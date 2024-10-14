a316c973fb6b076acd186bae1ff66ad9c1dcc739
John Samuel <johnsamuelwrites@gmail.com>
2024-10-14 16:14:32 +0200

    Update examples and add reference

--- examples/arithmetic.py
+++ examples/arithmetic.py
@@ -1,3 +1,12 @@
+#
+# SPDX-FileCopyrightText: 2024 John Samuel <johnsamuelwrites@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+
+# Reference of numeral systems: https://en.wikipedia.org/wiki/List_of_numeral_systems
+
 from multilingualprogramming.numeral.mp_numeral import MPNumeral
 
 ## Roman numerals
@@ -15,6 +24,25 @@ print(num1**num2)
 ## Unicode numerals
 num1 = MPNumeral("١٢٣٤٥")
 num2 = MPNumeral("൧൩")
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1**num2)
+
+
+num1 = MPNumeral("߉")
+num2 = MPNumeral("߂")
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1**num2)
+
+num1 = MPNumeral("𐒡")
+num2 = MPNumeral("𐒣")
 print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
